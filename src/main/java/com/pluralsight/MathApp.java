9cc9a248a1582d00305db77d42acabc7bbecfdd6
Kylee <kkeith.biz@gmail.com>
2025-09-24 10:22:01 -0400

    Vehicals Prices Added

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -7,5 +7,14 @@ public class MathApp {
     double garySalary = 80050.00;
     double highestSalary = Math.max(bobSalary, garySalary);
     System.out.println(highestSalary);
+
+
+    // Vehicals Prices
+
+        double carPrice = 28005.56;
+        double truckPrice = 74054.56;
+        double lowestPrice = Math.min(carPrice, truckPrice);
+        System.out.println(lowestPrice);
+
     }
 }
