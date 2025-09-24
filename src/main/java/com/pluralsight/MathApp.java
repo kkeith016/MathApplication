9b68f57ca4202616084a5eca8b1b8017b6285c09
Kylee <kkeith.biz@gmail.com>
2025-09-24 10:18:16 -0400

    Salarys Defined.

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -3,5 +3,9 @@ package com.pluralsight;
 public class MathApp {
     public static void main(String[] args) {
 
+    double bobSalary = 52500.00;
+    double garySalary = 80050.00;
+    double highestSalary = Math.max(bobSalary, garySalary);
+    System.out.println(highestSalary);
     }
 }
